628e929d26b83683cc148bd42c632021cec0f8d5
Tenyo22 <ematen@outlook.com>
2023-12-02 13:24:34 -0600

    Implements exit function with materialUI

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import { IconButton, Menu, MenuItem } from '@mui/material'
+import React, { useState } from 'react'
+import { deleteToken } from '../js/auth/token'
 
 const Navbar = ({ home, periodo, usuario }) => {
+
+    const [anchorEl, setAnchorEl] = useState(null)
+
+    const handleMenu = (event) => setAnchorEl(event.currentTarget)
+    const handleClose = () => setAnchorEl(null)
+
+    const handleExit = () => {
+        setAnchorEl(null)
+        deleteToken()
+    }
 
     return (
         <>
@@ -15,9 +27,35 @@ const Navbar = ({ home, periodo, usuario }) => {
                         </h4>
                     </div>
                     <h5 className='d-flex align-items-center justify-content-arround '>
-                        {usuario} 
-                        <i className='bi bi-person-circle fs-1 ps-2'></i>
-                        </h5>
+                        {usuario}
+                        <div>
+                            <IconButton
+                                size='large'
+                                aria-label='account of current user'
+                                aria-controls='menu-appbar'
+                                aria-haspopup='true'
+                                onClick={handleMenu}
+                                color='inherit'>
+                                <i className='bi bi-person-circle fs-1'></i>
+                            </IconButton>
+                            <Menu
+                                id='menu-appbar'
+                                anchorEl={anchorEl}
+                                anchorOrigin={{
+                                    vertical: 'bottom',
+                                    horizontal: 'left',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'left',
+                                }}
+                                open={Boolean(anchorEl)}
+                                onClose={handleClose}>
+                                <MenuItem onClick={handleExit}>Exit</MenuItem>
+                            </Menu>
+                        </div>
+                    </h5>
                 </div>
             </nav>
         </>
