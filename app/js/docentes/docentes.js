56f22b7c4baa290cba34ca4bd79f0442516713c6
Tenyo22 <ematen@outlook.com>
2023-12-06 11:43:51 -0600

    Logic Materias, evidencias by docente

---
+++ app/js/docentes/docentes.js
@@ -0,0 +1,116 @@
+const { getId } = require("../auth/token");
+const { default: apiConfig } = require("../config/apiConfig");
+const { getMaterias } = require("../jefedivision/materias");
+
+const API = apiConfig.apiDocentes
+const token = localStorage.getItem("token");
+
+let idDocente = 0
+let docente = []
+let allMaterias = []
+let materiasDocente = []
+let materias = []
+let tipoEvidencias = []
+
+module.exports.getAllMaterias = async () => {
+    const { materias } = await getMaterias()
+    allMaterias = materias
+}
+
+module.exports.getDocente = async () => {
+    idDocente = getId()
+    try {
+        const result = await fetch(`${API}/docentes/usuario/${idDocente}`, {
+            headers: {
+                'Authorization': "Bearer " + token,
+                'Content-Type': 'application/json',
+            }
+        })
+        if (result.ok) {
+            if (result.status === 200) {
+                const data = await result.json()
+                docente = data
+                // console.log(data)
+            }
+        }
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+module.exports.getMateriasDocente = async () => {
+    try {
+        const result = await fetch(`${API}/materiadocente/${docente.id}`, {
+            headers: {
+                "Authorization": "Bearer" + token,
+                "Content-Type": "application/json",
+            }
+        })
+        if (result.ok) {
+            const data = await result.json()
+            materiasDocente = data
+            // console.log(data)
+        }
+
+    } catch (e) {
+        console.error(e)
+    }
+}
+
+module.exports.filterMaterias = () => {
+    materias = []
+    // console.log(allMaterias)
+    materiasDocente.map(asig => {
+        const clave = asig.clave_materia
+        const materiaEncontrada = allMaterias.find(mat => mat.clave_materia === clave)
+
+        if (materiaEncontrada) {
+            materias.push({
+                clave: clave,
+                materia: materiaEncontrada.nombre_materia,
+                semestre: materiaEncontrada.semestre,
+            })
+        }
+    })
+    materias.sort((a, b) => parseInt(a.semestre, 10) - parseInt(b.semestre, 10))
+
+    const abreviaturaSemestre = (numero) => {
+        const abreviaturas = ['1ro', '2do', '3ro', '4to', '5to', '6to', '7mo', '8vo', '9no']
+        return abreviaturas[numero - 1] || `${numero}to`
+    }
+
+    const materiasSemestre = materias.reduce((acc, materia) => {
+        const { semestre, materia: nombreMateria, clave } = materia
+        const abreviatura = abreviaturaSemestre(Number(semestre))
+
+        acc[abreviatura] = acc[abreviatura] || []
+        acc[abreviatura].push({ clave, nombreMateria })
+        return acc
+    }, {})
+
+    // console.log(materiasSemestre)
+    // console.log(materias)
+    return { materiasSemestre }
+}
+
+module.exports.getTipoEvidencias = async () => {
+    tipoEvidencias = []
+    try {
+        const result = await fetch(`${API}/evidencias`, {
+            headers: {
+                'Authorization': 'Bearer ' + token,
+                'Content-Type': 'application/json',
+            }
+        })
+        if (result.ok) {
+            if (result.status === 200) {
+                const data = await result.json()
+                tipoEvidencias = data
+                return { tipoEvidencias }
+                // console.log(tipoEvidencias)
+            }
+        }
+    } catch (e) {
+        console.error(e)
+    }
+}
