14f6c7e8f6f2c4d49d8df03481177d0edb306809
Tenyo22 <ematen@outlook.com>
2023-12-11 21:10:20 -0600

    Ask plan estudio - change periodo, create a new plan estudio

--- app/js/jefedivision/plan-estudios.js
+++ app/js/jefedivision/plan-estudios.js
@@ -1,6 +1,7 @@
 const { Container, Grid, Paper } = require("@mui/material");
 const { default: apiConfig } = require("../config/apiConfig");
 const { getMateriasByPlanEstudios } = require("./materias");
+const { default: Swal } = require("sweetalert2");
 
 // const API = "http://localhost:8081"
 const API = apiConfig.apiMaterias
@@ -28,17 +29,21 @@ module.exports.getPlanEstudios = async (setPlanEstudios) => {
 }
 
 module.exports.getPlanActualEstudios = async () => {
-    const result = await fetch(`${API}/planestudios`, {
-        headers: {
-            'Authorization': 'Bearer ' + token,
-            'Content-Type': 'application/json',
-        },
-    })
-    if (result.ok) {
-        const data = await result.json()
-        const planActual = data[0].clave
-        // console.log(planActual)
-        return { planActual }
+    try {
+        const result = await fetch(`${API}/planestudios`, {
+            headers: {
+                'Authorization': 'Bearer ' + token,
+                'Content-Type': 'application/json',
+            },
+        })
+        if (result.ok) {
+            const data = await result.json()
+            const planActual = data[0].clave
+            // console.log(planActual)
+            return { planActual }
+        }
+    } catch (e) {
+        console.error(e)
     }
 }
 
@@ -48,13 +53,6 @@ module.exports.getMateriasPlanEstudio = async (clave) => {
     materias = data
     console.log(materias)
 }
-
-// module.exports.getClasificacion = async () => {
-//     competencias = []
-//     const { competencia } = await getClasificacion()
-//     competencias = competencia
-//     console.log(competencias)
-// }
 
 module.exports.Reticula = () => {
     return (
@@ -87,4 +85,104 @@ module.exports.Reticula = () => {
             ))}
         </Grid>
     )
+}
+
+module.exports.askPlanEstudios = async (plan) => {
+    const result = await Swal.fire({
+        icon: 'question',
+        title: 'El Plan de Estudios sigue siendo el mismo?',
+        confirmButtonText: "Sí",
+        confirmButtonColor: '#2979FF',
+        showDenyButton: true,
+        denyButtonText: "Cambiar",
+        denyButtonColor: '#41c300',
+        showCancelButton: true,
+    })
+    if (result.isConfirmed) {
+        // console.log(result.value)
+        // console.log(plan)
+        return plan
+    } else if (result.isDenied) {
+        // console.log(result.value)
+        const { value: newPlan } = await Swal.fire({
+            title: 'Ingrese el nuevo plan de estudios',
+            input: 'text',
+            inputPlaceholder: 'ISIC-2010-102',
+            showCancelButton: true,
+            confirmButtonText: 'Ok',
+            inputAutoTrim: true,
+            inputAttributes: {
+                maxLength: "15",
+            },
+            preConfirm: (inputValue) => {
+                return inputValue.toUpperCase()
+            }
+        })
+        if (newPlan) {
+            // console.log(newPlan)
+            return newPlan
+        } else {
+            await Swal.fire({
+                icon: 'info',
+                title: 'Plan No valido',
+            })
+        }
+    }
+    return false
+}
+
+module.exports.deletePlanEstudio = async (plan) => {
+    try {
+        const result = await fetch(`${API}/planestudios/${plan}`, {
+            method: 'DELETE',
+            headers: {
+                'Authorization': 'Bearer ' + token,
+                'Content-Type': 'application/json',
+            },
+        })
+        if (result.ok) {
+            if (result.status === 200) {
+                console.log('OK')
+            }
+        }
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+module.exports.createPlanEstudio = async (plan) => {
+    // console.log(plan)
+    const obj = {
+        clave: plan,
+        status: 'y'
+    }
+    // console.log(obj)
+    try {
+        const result = await fetch(`${API}/planestudios`, {
+            method: 'POST',
+            headers: {
+                'Authorization': 'Bearer ' + token,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(obj)
+        })
+        if (result.ok) {
+            if (result.status === 201) {
+                await Swal.fire({
+                    icon: 'success',
+                    title: 'Plan de estudios creado!'
+                })
+            } else {
+                const mensaje = await result.json()
+                Swal.fire({
+                    icon: 'info',
+                    title: mensaje.mensaje
+                })
+                return false
+            }
+        }
+    } catch (e) {
+        console.error(e)
+    }
+    return true
 }
