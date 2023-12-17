f5f312eb78b2c4b251fcf9ef4b6df758f874df75
Tenyo22 <ematen@outlook.com>
2023-12-16 18:22:44 -0600

    Show avance for invitados

---
+++ app/invitado/page.jsx
@@ -0,0 +1,14 @@
+'use client'
+
+import React, { useEffect, useState } from 'react'
+import Avance from '../components/Avance'
+
+const invitado = () => {
+    return (
+        <>
+            <Avance />
+        </>
+    )
+}
+
+export default invitado
