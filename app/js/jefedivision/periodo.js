fadf9262ae6407eb186d686d96dd4dc4186e9489
Tenyo22 <ematen@outlook.com>
2023-12-16 19:21:18 -0600

    Ask periodo, create and delete periodo

--- app/js/jefedivision/periodo.js
+++ app/js/jefedivision/periodo.js
@@ -1,3 +1,4 @@
+const { default: Swal } = require("sweetalert2");
 const { default: apiConfig } = require("../config/apiConfig");
 
 // const API = "http://localhost:8081"
@@ -22,5 +23,97 @@ module.exports.getPeriodo = async () => {
     } catch (error) {
         console.error(error);
         return { periodoActual: null, periodosList: [] }
+    }
+}
+
+module.exports.askPeriodo = async () => {
+    const { isDismissed: result, value: newPeriodo } = await Swal.fire({
+        icon: 'question',
+        title: 'Ingrese el nuevo periodo!',
+        input: 'text',
+        inputPlaceholder: '2021-VER',
+        inputAutoTrim: true,
+        showCancelButton: true,
+        confirmButtonText: 'Crear',
+        inputAttributes: {
+            maxLength: '8',
+        },
+        preConfirm: (inputValue) => {
+            return inputValue.toUpperCase();
+        }
+    })
+    // console.log(result)
+    if (result) return false
+    if (newPeriodo) {
+        return newPeriodo
+    } else {
+        await Swal.fire({
+            icon: 'info',
+            title: 'Periodo No Valido!',
+        })
+    }
+    return false
+}
+
+module.exports.createPeriodo = async (periodo, plan, especialidad) => {
+    // console.log(periodo, plan, especialidad)
+    const data = {
+        "year": `${periodo}`,
+        "planEstudios": {
+            "clave": `${plan}`
+        },
+        "especialidad": {
+            "clave": `${especialidad}`
+        },
+        "status": "y"
+    }
+    
+    try {
+        const result = await fetch(`${API}/periodo`, {
+            method: "POST",
+            headers: {
+                'Authorization': 'Bearer ' + token,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+        if (result.ok) {
+            if (result.status === 201) {
+                await Swal.fire({
+                    icon: 'success',
+                    title: 'Periodo creado!',
+                })
+                return true
+            } else {
+                const mensaje = await result.json()
+                await Swal.fire({
+                    icon: 'info',
+                    title: mensaje.mensaje,
+                })
+            }
+        }
+        return false
+    } catch (e) {
+        console.error(e)
+    }
+}
+
+module.exports.deletePeriodo = async (periodo) => {
+    console.log(periodo)
+    try {
+        const result = await fetch(`${API}/periodo/${periodo}`, {
+            method: 'DELETE',
+            headers: {
+                'Authorization': 'Bearer ' + token,
+                'Content-Type': 'application/json',
+            },
+        })
+        if (result.ok) {
+            if (result.status === 200) {
+
+            }
+        }
+    } catch (e) {
+        console.error(e)
     }
 }
